d611fd217e32fcf034c24f3d62fed85cd8c289f9
Chunqi SHI <chqshi@gmail.com>
2015-12-21 14:41:22 +0800

    added testcases

---
+++ src/test/java/edu/brandeis/cs/gate/GateAnnieTokenizerTest.java
@@ -0,0 +1,38 @@
+package edu.brandeis.cs.gate;
+
+import edu.brandeis.cs.service.AbstractWebService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+* GateAnnieTokenizer Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ʮ���� 21, 2015</pre> 
+* @version 1.0 
+*/ 
+public class GateAnnieTokenizerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: execute(Container json) 
+* 
+*/ 
+@Test
+public void testExecute() throws Exception {
+    AbstractWebService ws = new GateAnnieTokenizer();
+    String res = ws.execute("How are you today? Fine thank you.");
+    System.out.println(res);
+} 
+
+
+} 
